d20fdf5d743198dfb97276789f1e227d58838e5f
Zuppa93 <36716382+Zuppa93@users.noreply.github.com>
2018-12-01 18:00:29 +0100

    Create TEXT.h

---
+++ TEXT.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "EVKD.h"
+#include <iostream>
+
+class TEXT {
+	private:
+		EVKD * Start[99];
+		int AnzTexte;
+
+	public:
+		TEXT();
+		//TEXT(EVKD *N);
+		void anhaenge(char * ln);
+		//int strLen(char * text);
+		void einfuegeSortiert(EVKD * ln,int MAX);
+		EVKD * loesche(int Pos);
+		void zeigeDich();
+		void iSort();
+
+		//////////////////////
+		// Custom Functions //
+		//////////////////////
+
+		int getAnzTexte();
+		void decrementAnzTexte();
+
+		void reassambleArrayPlus(int  reap,int MAX);
+		void reassambleArrayMinus(int reap,int MAX);
+};
